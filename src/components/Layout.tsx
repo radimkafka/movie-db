3e6252a4e0d60b821b4e6379c8d1cfdc5141af04
Radim Kafka <kafkaradim@gmail.com>
2023-08-21 19:45:16 +0200

    minor changes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,9 @@
-import { AppBar, Box, Button, IconButton, Toolbar } from "@mui/material";
+import { AppBar, Box, Button, IconButton, Link, Toolbar } from "@mui/material";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useColorMode } from "../providers/ColorModeProvider";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Layout = () => {
   const colorMode = useColorMode();
@@ -24,6 +25,13 @@ const Layout = () => {
             </Button>
           </Box>
 
+          <Box>
+            <Link href="https://github.com/radimkafka/movie-db" target="_blank" rel="noreferrer">
+              <IconButton color="default">
+                <GitHubIcon />
+              </IconButton>
+            </Link>
+          </Box>
           <Box>
             <IconButton sx={{ ml: 1 }} onClick={colorMode.toggle} color="inherit">
               {colorMode.mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
